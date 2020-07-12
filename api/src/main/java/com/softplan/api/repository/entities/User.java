8740e64992588ccb027afe704b04d0469cde10ef
reginaldo <camilo.melges@gmail.com>
2020-07-12 19:54:11 -0400

    added gitignore global and update readme

---
+++ api/src/main/java/com/softplan/api/repository/entities/User.java
@@ -0,0 +1,31 @@
+package com.softplan.api.repository.entities;
+
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String email;
+    private String password;
+}
